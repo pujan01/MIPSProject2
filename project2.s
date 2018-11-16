dc7e7839cf950865efd7e025e859ad11b7602d98
pujan01 <thapapujan2@gmail.com>
2018-11-16 18:14:27 -0500

    first commit. this is storing the strings that i will use in my program. it will also reserve space for the input string

---
+++ project2.s
@@ -0,0 +1,5 @@
+.data
+	inputLong: .asciiz "Input is too long."
+	emptyInput: .asciiz "Input is empty."
+	invalidNumber: .asciiz "Invalid base-30 number."
+	userInput: .space 1000	#reserving the 1000 bytes of memory for userInput
