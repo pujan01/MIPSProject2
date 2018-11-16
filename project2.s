d6f136b23569d96250fc7d466f0c8eeb390c940b
pujan01 <thapapujan2@gmail.com>
2018-11-16 18:16:01 -0500

    these lines help to add one more of those string, which will be crucial to store and process the base 30 number

--- project2.s
+++ project2.s
@@ -3,3 +3,6 @@
 	emptyInput: .asciiz "Input is empty."
 	invalidNumber: .asciiz "Invalid base-30 number."
 	userInput: .space 1000	#reserving the 1000 bytes of memory for userInput
+	fourCharacters: .space 4
+.text
+main:
