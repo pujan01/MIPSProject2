5a271e070f45438eb45ae380fa8135e13d67f350
pujan01 <thapapujan2@gmail.com>
2018-11-16 18:20:31 -0500

    i am now starting my first loop with label as length_loop, it will also store the value of first bit in $a0 register. in the next line, the value of $a1 is updated for next round of input

--- project2.s
+++ project2.s
@@ -13,3 +13,7 @@ main:
 	syscall
 	la $a1, userInput
 	li $t9, 0				#if it is 0, then a valid character is not found. 1 means a non-space character is found
+
+length_loop:
+	lb $a0,($a1)        			# read the character
+	addi $a1, $a1, 1
